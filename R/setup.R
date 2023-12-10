59a158179dd901c5066d054cdc351dbd1fbea7b9
SimonDeDeyne <simon2d@gmail.com>
2023-12-10 23:18:58 +1300

    Add presentation

---
+++ R/setup.R
@@ -0,0 +1,31 @@
+# General data wrangling
+library(tidyverse)
+library(magrittr)
+library(tictoc)
+
+#library(knitr)
+library(tictoc)
+library(broom)
+
+library(igraph)
+
+# Visualization
+library(tidygraph)
+library(viridis)
+library(ggraph)
+library(ggrepel)
+library(gridExtra)
+library(graphlayouts)
+
+library(comato) # Pathfinder graphs
+library(assortnet) # Assortativity Analysis
+
+# also required: psych
+
+# Settings
+set.seed(9375)
+options(scipen = 999)
+options(dplyr.summarise.inform = FALSE)
+options(readr.show_col_types = FALSE)
+
+
